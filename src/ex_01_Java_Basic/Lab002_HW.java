c4f463d8a90c51bdb0e1d743b73036e6b70f66b9
Ramesh <rameshrravuri@gmail.com>
2025-06-11 00:10:59 +0530

    printf class

---
+++ src/ex_01_Java_Basic/Lab002_HW.java
@@ -0,0 +1,27 @@
+package ex_01_Java_Basic;
+
+public class Lab002_HW {
+    public static void main (String[] args){
+ int a=9;
+ int b=1;
+        System.out.printf("%d * %d =%d",a,b,a*b);
+b++;
+        System.out.printf("%n%d * %d =%d",a,b,a*b);
+        b++;
+        System.out.printf("%n%d * %d =%d",a,b,a*b);
+        b++;
+        System.out.printf("%n%d * %d =%d",a,b,a*b);
+        b++;
+        System.out.printf("%n%d * %d =%d",a,b,a*b);
+        b++;
+        System.out.printf("%n%d * %d =%d",a,b,a*b);
+        b++;
+        System.out.printf("%n%d * %d =%d",a,b,a*b);
+        b++;
+        System.out.printf("%n%d * %d =%d",a,b,a*b);
+        b++;
+        System.out.printf("%n%d * %d =%d",a,b,a*b);
+b++;
+        System.out.printf("%n%d * %d =%d",a,b,a*b);
+    }
+}
