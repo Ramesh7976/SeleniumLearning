23c4b7c3007ae4c8aaca5147fbc98f069935fa7b
Ramesh <rameshrravuri@gmail.com>
2025-06-19 08:09:38 +0530

    printf class while loop

---
+++ src/test_programs/program_1.java
@@ -0,0 +1,26 @@
+package test_programs;
+
+public class program_1 {
+    public static void main(String[] args){
+boolean b=true;
+int a =5000;
+double d= 9.99;
+float f= 99.9f;
+char c= 'a';
+String s= "ABC";
+
+
+
+
+        System.out.println("Boolean: " + b);
+        System.out.println("Integer: " + a);
+        System.out.println("Double: " + d );
+        System.out.println("Float" +f);
+        System.out.println("Char: " +c);
+        System.out.println("String: " +s);
+
+
+
+
+    }
+}
