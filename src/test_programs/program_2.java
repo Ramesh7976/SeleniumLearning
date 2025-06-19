df0b3fe5bafb24513121c6f0e5bc39752adcf958
Ramesh <rameshrravuri@gmail.com>
2025-06-19 08:17:42 +0530

    printf class while loop

---
+++ src/test_programs/program_2.java
@@ -0,0 +1,12 @@
+package test_programs;
+
+public class program_2 {
+    public static void main(String[] args) {
+        System.out.println(10+10);
+        System.out.println(9*9);
+        System.out.println(19-8);
+        System.out.println(100/11);
+        System.out.println(98|8);
+
+    }
+}
