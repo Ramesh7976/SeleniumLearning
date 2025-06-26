47f28af8d01cca4732b8605da85e37973c1e35ae
Ramesh <rameshrravuri@gmail.com>
2025-06-26 08:26:29 +0530

    printf class while loop

---
+++ src/test_programs/palindromeTest.java
@@ -0,0 +1,10 @@
+package test_programs;
+
+public class palindromeTest {
+    public static void main(String[] args) {
+        //Write a Java program to check if a given string is a palindrome. Consider only alphanumeric characters and ignore case.
+        //Input is: A man, a plan, a canal: Panama : true (💡 Explanation:Ignoring spaces and punctuation, it reads the same forwards and backwards)
+//race a car : false( Explanation:Not a palindrome when considering only alphanumeric characters)
+
+    }
+}
